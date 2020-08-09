115c504b736264cfc80ece25e3531700d2f78a13
Qraksa <filipsztajn@gmail.com>
2020-08-09 23:01:38 +0200

    Adres klienta

--- Danedoformularza.js
+++ Danedoformularza.js
@@ -3,7 +3,6 @@ const username = document.getElementById("username");
 const Lastname = document.getElementById("Nazwisko");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -40,8 +39,29 @@ function checkInputs() {
 
   if (passwordValue === "") {
     setErrorFor(password, "To pole nie może być puste");
+  } else if (!isTelefon(passwordValue)) {
+    setErrorFor(password, "Niepoprawny Numer Telefonu");
   } else {
     setSuccessFor(password);
+  }
+  var usernamecheck = username.parentNode.className;
+  var Lastnamecheck = Lastname.parentNode.className;
+  var emailcheck = email.parentNode.className;
+  var passwordcheck = password.parentNode.className;
+  console.log(usernamecheck);
+  var Dalej = document.getElementById("dalejdostawa");
+  if (
+    usernamecheck == "form-control2 success" &&
+    Lastnamecheck == "form-control2 success" &&
+    emailcheck == "form-control2 success" &&
+    passwordcheck == "form-control2 success"
+  ) {
+    Dalej.addEventListener("click", function () {
+      var container = document.getElementById("container");
+      container.style.display = "none";
+      var container2 = document.getElementById("container2");
+      container2.style.display = "block";
+    });
   }
 }
 
@@ -60,5 +80,74 @@ function setSuccessFor(input) {
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
+  );
+}
+
+/*Dostawa*/
+
+const form2 = document.getElementById("form2");
+const Kraj = document.getElementById("Kraj");
+const Miasto = document.getElementById("Miasto");
+const Kodpocztowy = document.getElementById("Kod pocztowy");
+const Ulicainumerbudynku = document.getElementById("Ulica i numer budynku");
+
+form2.addEventListener("submit", (i) => {
+  i.preventDefault();
+
+  checkInput();
+});
+
+function checkInput() {
+  const KrajValue = Kraj.value.trim();
+  const MiastoValue = Miasto.value.trim();
+  const KodpocztowyValue = Kodpocztowy.value.trim();
+  const UlicainumerbudynkuValue = Ulicainumerbudynku.value.trim();
+
+  if (KrajValue === "") {
+    setErrorFor(Kraj, "To pole nie może być puste");
+  } else {
+    setSuccessFor(Kraj);
+  }
+
+  if (MiastoValue === "") {
+    setErrorFor(Miasto, "To pole nie może być puste");
+  } else {
+    setSuccessFor(Miasto);
+  }
+
+  if (KodpocztowyValue === "") {
+    setErrorFor(Kodpocztowy, "To pole nie może być puste");
+  } else if (!isKodpocztowy(KodpocztowyValue)) {
+    setErrorFor(Kodpocztowy, "Niepoprawny kod pocztowy");
+  } else {
+    setSuccessFor(Kodpocztowy);
+  }
+
+  if (UlicainumerbudynkuValue === "") {
+    setErrorFor(Ulicainumerbudynku, "To pole nie może być puste");
+  } else {
+    setSuccessFor(Ulicainumerbudynku);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl2 = input.parentElement;
+  const small = formControl2.querySelector("small");
+  formControl2.className = "form-control2 error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl2 = input.parentElement;
+  formControl2.className = "form-control2 success";
+}
+
+function isKodpocztowy(Kodpocztowy) {
+  return /^((\d){2})-((\d){3})$/.test(Kodpocztowy);
+}
+
+function isTelefon(password) {
+  return /(^((\d){3})-((\d){3})-((\d){3})$)|(^((\d){3})((\d){3})((\d){3})$)|(^((\d){3}) ((\d){3}) ((\d){3})$)/.test(
+    password
   );
 }
